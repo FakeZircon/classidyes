81255855f5b2a237dd2709e5b9eec11933b276fb
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-05-15 13:21:50 -0600

    Initial commit

---
+++ src/client/java/fakezircon/classidyes/ClassidyesClient.java
@@ -0,0 +1,10 @@
+package fakezircon.classidyes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClassidyesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
