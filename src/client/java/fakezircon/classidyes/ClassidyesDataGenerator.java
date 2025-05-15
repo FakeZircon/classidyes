e0ccdc3ffaadd9ae49535065cfc90fb2ff10a35f
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-05-15 14:16:58 -0600

    configured datagen for item models

--- src/client/java/fakezircon/classidyes/ClassidyesDataGenerator.java
+++ src/client/java/fakezircon/classidyes/ClassidyesDataGenerator.java
@@ -1,11 +1,34 @@
 package fakezircon.classidyes;
 
+import fakezircon.classidyes.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
 
 public class ClassidyesDataGenerator implements DataGeneratorEntrypoint {
 	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+	public void onInitializeDataGenerator(FabricDataGenerator generator) {
+		FabricDataGenerator.Pack pack = generator.createPack();
+		pack.addProvider(ModelGenerator::new);
+	}
 
+	public static class ModelGenerator extends FabricModelProvider {
+		public ModelGenerator(FabricDataOutput output) {
+			super(output);
+		}
+
+		@Override
+		public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+			// ...
+		}
+
+		@Override
+		public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+			itemModelGenerator.register(ModItems.TEST, Models.GENERATED);
+		}
 	}
 }
