d43f90a10c27fc42700f501aa9f394cbd2841cfa
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-05-15 13:28:45 -0600

    added data generation

---
+++ src/client/java/fakezircon/classidyes/ClassidyesDataGenerator.java
@@ -0,0 +1,11 @@
+package fakezircon.classidyes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ClassidyesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
