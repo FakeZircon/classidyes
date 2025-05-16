fd5b3eda724e029236804182ed3023ed5f17cc03
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-05-15 19:46:03 -0600

    added quick register for flowers

--- src/main/java/fakezircon/classidyes/block/ModBlocks.java
+++ src/main/java/fakezircon/classidyes/block/ModBlocks.java
@@ -5,6 +5,7 @@ import fakezircon.classidyes.item.ModItems;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -15,16 +16,8 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-    public static final Block AQUAMARINE_HYDRANGEA = registerBlock(
-            new FlowerBlock(StatusEffects.ABSORPTION, 10, FabricBlockSettings.copyOf(Blocks.ALLIUM).nonOpaque().noCollision()),
-            "aquamarine_hydrangea",
-            true
-    );
-    public static final Block POTTED_AQUAMARINE_HYDRANGEA = registerBlock(
-            new FlowerPotBlock(AQUAMARINE_HYDRANGEA, FabricBlockSettings.copyOf(Blocks.POTTED_ALLIUM).nonOpaque()),
-            "potted_aquamarine_hydrangea",
-            false
-    );
+    public static final Block AQUAMARINE_HYDRANGEA = registerFlower("aquamarine_hydrangea", StatusEffects.ABSORPTION, 200);
+    public static final Block POTTED_AQUAMARINE_HYDRANGEA = registerPottedFlower(AQUAMARINE_HYDRANGEA);
 
     public static Block registerBlock(Block block, String name, boolean shouldRegisterItem){
         Identifier id = new Identifier(Classidyes.MOD_ID, name);
@@ -35,6 +28,23 @@ public class ModBlocks {
         }
 
         return Registry.register(Registries.BLOCK, id, block);
+    }
+
+    //quick register for flowers
+    public static Block registerFlower(String name, StatusEffect effect, int duration){
+        return registerBlock(
+                new FlowerBlock(effect, duration, FabricBlockSettings.copyOf(Blocks.ALLIUM)),
+                name,
+                true
+        );
+    }
+    //quick register for potted flowers
+    public static Block registerPottedFlower(Block flower){
+        return registerBlock(
+                new FlowerPotBlock(flower, FabricBlockSettings.copyOf(Blocks.POTTED_ALLIUM)),
+                "potted_" + flower.getTranslationKey().split("\\.")[2],
+                false
+        );
     }
 
     public static void registerModBlocks(){
