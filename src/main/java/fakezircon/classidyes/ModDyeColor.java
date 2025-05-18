092097ba224aba0bd951720ddc2176915323bb75
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-05-17 18:43:19 -0600

    cleaned up the mod dye color enum/class
    still doesn't work, whoop-dee-do!
    DDDyes did not hold the answers for me

--- src/main/java/fakezircon/classidyes/ModDyeColor.java
+++ src/main/java/fakezircon/classidyes/ModDyeColor.java
@@ -1,28 +1,13 @@
 package fakezircon.classidyes;
 
-import com.google.common.base.Preconditions;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.minecraft.block.MapColor;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.StringIdentifiable;
-import net.minecraft.util.function.ValueLists;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Arrays;
-import java.util.Map;
-import java.util.function.IntFunction;
-import java.util.stream.Collectors;
 
 public enum ModDyeColor implements StringIdentifiable {
 //    BLACK(15, "black", 1908001, MapColor.BLACK, 1973019, 0); //template
-    CHARTREUSE(3, "chartreuse", 0x97f738, MapColor.EMERALD_GREEN, 0x97f738, 0x97f738);
+    CHARTREUSE(18, "chartreuse", 0x97f738, MapColor.EMERALD_GREEN, 0x97f738, 0x97f738);
 
-    private static final IntFunction<ModDyeColor> BY_ID = ValueLists.createIdToValueFunction(ModDyeColor::getId, values(), ValueLists.OutOfBoundsHandling.ZERO);
-    private static final Int2ObjectOpenHashMap<ModDyeColor> BY_FIREWORK_COLOR = new Int2ObjectOpenHashMap<>(
-            (Map<? extends Integer, ? extends ModDyeColor>)Arrays.stream(values()).collect(Collectors.toMap(color -> color.fireworkColor, color -> color))
-    );
-    public static final StringIdentifiable.Codec<ModDyeColor> CODEC = StringIdentifiable.createCodec(ModDyeColor::values);
     private final int id;
     private final String name;
     private final MapColor mapColor;
@@ -42,83 +27,28 @@ public enum ModDyeColor implements StringIdentifiable {
         this.fireworkColor = fireworkColor;
     }
 
-    /**
-     * {@return the integer ID of the dye color}
-     */
     public int getId() {
         return this.id;
     }
 
-    /**
-     * {@return the name of the dye color}
-     */
     public String getName() {
         return this.name;
     }
 
-    /**
-     * {@return the float array containing the red, green and blue components of this dye color}
-     *
-     * <p>Each value of the array is between {@code 0.0} and {@code 255.0} (both inclusive).
-     */
     public float[] getColorComponents() {
         return this.colorComponents;
     }
 
-    /**
-     * {@return the corresponding map color}
-     */
     public MapColor getMapColor() {
         return this.mapColor;
     }
 
-    /**
-     * {@return the color used for colored fireworks as RGB integer}
-     *
-     * <p>The returned value is between {@code 0} and {@code 0xFFFFFF}.
-     */
     public int getFireworkColor() {
         return this.fireworkColor;
     }
 
-    /**
-     * {@return the color used for dyed signs as RGB integer}
-     *
-     * <p>The returned value is between {@code 0} and {@code 0xFFFFFF}.
-     */
     public int getSignColor() {
         return this.signColor;
-    }
-
-    /**
-     * {@return the dye color whose ID is {@code id}}
-     *
-     * @apiNote If out-of-range IDs are passed, this returns {@link #WHITE}.
-     */
-    public static ModDyeColor byId(int id) {
-        return (ModDyeColor)BY_ID.apply(id);
-    }
-
-    /**
-     * {@return the dye color whose name is {@code name}, or {@code defaultColor} if
-     * there is no such color}
-     *
-     * @apiNote This returns {@code null} only if {@code defaultColor} is {@code null}.
-     */
-    @Nullable
-    @Contract("_,!null->!null;_,null->_")
-    public static ModDyeColor byName(String name, @Nullable ModDyeColor defaultColor) {
-        ModDyeColor dyeColor = (ModDyeColor)CODEC.byId(name);
-        return dyeColor != null ? dyeColor : defaultColor;
-    }
-
-    /**
-     * {@return the dye color whose firework color is {@code color}, or {@code null}
-     * if there is no such color}
-     */
-    @Nullable
-    public static ModDyeColor byFireworkColor(int color) {
-        return BY_FIREWORK_COLOR.get(color);
     }
 
     public String toString() {
--- src/main/java/fakezircon/classidyes/mixin/DyeColorsMixin.java
+++ src/main/java/fakezircon/classidyes/mixin/DyeColorsMixin.java
@@ -1,13 +1,28 @@
 package fakezircon.classidyes.mixin;
 
+import net.minecraft.block.MapColor;
 import net.minecraft.util.DyeColor;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DyeColor.class)
 public class DyeColorsMixin {
+    @Shadow
+    @Mutable
+    @Final
+    private static DyeColor[] $VALUES;
+
+    @Invoker("<init>")
+    public static DyeColor newColor(int id, String name, int color, MapColor mapColor, int fireworkColor, int signColor){
+        throw new AssertionError();
+    }
+
     @Inject(method = "<init>", at = @At(value = "FIELD", target = ""))
     private void init(CallbackInfo info) {
         // This code is injected into the start of MinecraftServer.loadWorld()V
