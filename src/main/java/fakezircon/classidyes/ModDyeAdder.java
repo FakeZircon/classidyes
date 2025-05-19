7a5d8c44af0cbf9023e0b521b5a286aec58ae903
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-05-19 00:36:11 -0600

    verified early riser works
    read the fabric asm page more and figure out the enum builders

--- src/main/java/fakezircon/classidyes/ModDyeAdder.java
+++ src/main/java/fakezircon/classidyes/ModDyeAdder.java
@@ -1,5 +1,8 @@
 package fakezircon.classidyes;
 
-public class ModDyeAdder {
-
+public class ModDyeAdder implements Runnable{
+    @Override
+    public void run(){
+        Classidyes.LOGGER.info("Thank God. It Works");
+    }
 }
