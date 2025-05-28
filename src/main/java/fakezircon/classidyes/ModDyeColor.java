6f5ad42c6e96fabb83eaf4365368b642eb28c390
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-05-28 13:55:34 -0600

    added variable ModDyeColor indexing
    still gotta find where getRgbColor is called. and figure out duck typing a little better

--- src/main/java/fakezircon/classidyes/ModDyeColor.java
+++ src/main/java/fakezircon/classidyes/ModDyeColor.java
@@ -4,24 +4,26 @@ import net.minecraft.block.MapColor;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.StringIdentifiable;
 
+import static fakezircon.classidyes.util.MiscVals.dyeIndex;
+
 public enum ModDyeColor implements StringIdentifiable {
 //    BLACK(15, "black", 1908001, MapColor.BLACK, 1973019, 0); //template
-    CANDY_APPLE(16, "candy_apple", 0xf73838, MapColor.BRIGHT_RED, 0xf73838, 0xf73838),
-    CLEMENTINE(17, "clementine", 0xf79738, MapColor.ORANGE, 0xf79738, 0xf79738),
-    LEMON(18, "lemon", 0xf7f738, MapColor.YELLOW, 0xf7f738, 0xf7f738),
-    CHARTREUSE(19, "chartreuse", 0x97f738, MapColor.EMERALD_GREEN, 0x97f738, 0x97f738),
-    HARLEQUIN(20, "harlequin", 0x34e834, MapColor.PALE_GREEN, 0x34e834, 0x34e834),
-    SEAFOAM(21, "seafoam", 0x38f797, MapColor.BRIGHT_TEAL, 0x38f797, 0x38f797),
-    ROBINS_EGG(22, "robins_egg", 0x38f7f7, MapColor.DIAMOND_BLUE, 0x38f7f7, 0x38f7f7),
-    DENIM(23, "denim", 0x74b6f7, MapColor.DARK_AQUA, 0x74b6f7, 0x74b6f7),
-    PIGEON_BLUE(24, "pigeon_blue", 0x8686f7, MapColor.TERRACOTTA_BLUE, 0x8686f7, 0x8686f7),
-    SEANCE(25, "seance", 0x9738f7, MapColor.PURPLE, 0x9738f7, 0x9738f7),
-    BYZANTIUM(26, "byzantium", 0xc252f7, MapColor.MAGENTA, 0xc252f7, 0xc252f7),
-    NULL_PINK(27, "null_pink", 0xf738f7, MapColor.MAGENTA, 0xf738f7, 0xf738f7),
-    CERISE(28, "cerise", 0xf73897, MapColor.TERRACOTTA_PINK, 0xf73897, 0xf73897),
-    GUNMETAL(29, "gunmetal", 0x5a5a5a, MapColor.TERRACOTTA_GRAY, 0x5a5a5a, 0x5a5a5a),
-    APPARITION(30, "apparition", 0xa2a2a2, MapColor.TERRACOTTA_LIGHT_GRAY, 0xa2a2a2, 0xa2a2a2),
-    NACRE(31, "nacre", 0xf7f7f7, MapColor.OFF_WHITE, 0xf7f7f7, 0xf7f7f7);
+    CANDY_APPLE(dyeIndex, "candy_apple", 0xf73838, MapColor.BRIGHT_RED, 0xf73838, 0xf73838),
+    CLEMENTINE(dyeIndex + 1, "clementine", 0xf79738, MapColor.ORANGE, 0xf79738, 0xf79738),
+    LEMON(dyeIndex + 2, "lemon", 0xf7f738, MapColor.YELLOW, 0xf7f738, 0xf7f738),
+    CHARTREUSE(dyeIndex + 3, "chartreuse", 0x97f738, MapColor.EMERALD_GREEN, 0x97f738, 0x97f738),
+    HARLEQUIN(dyeIndex + 4, "harlequin", 0x34e834, MapColor.PALE_GREEN, 0x34e834, 0x34e834),
+    SEAFOAM(dyeIndex + 5, "seafoam", 0x38f797, MapColor.BRIGHT_TEAL, 0x38f797, 0x38f797),
+    ROBINS_EGG(dyeIndex + 6, "robins_egg", 0x38f7f7, MapColor.DIAMOND_BLUE, 0x38f7f7, 0x38f7f7),
+    DENIM(dyeIndex + 7, "denim", 0x74b6f7, MapColor.DARK_AQUA, 0x74b6f7, 0x74b6f7),
+    PIGEON_BLUE(dyeIndex + 8, "pigeon_blue", 0x8686f7, MapColor.TERRACOTTA_BLUE, 0x8686f7, 0x8686f7),
+    SEANCE(dyeIndex + 9, "seance", 0x9738f7, MapColor.PURPLE, 0x9738f7, 0x9738f7),
+    BYZANTIUM(dyeIndex + 10, "byzantium", 0xc252f7, MapColor.MAGENTA, 0xc252f7, 0xc252f7),
+    NULL_PINK(dyeIndex + 11, "null_pink", 0xf738f7, MapColor.MAGENTA, 0xf738f7, 0xf738f7),
+    CERISE(dyeIndex + 12, "cerise", 0xf73897, MapColor.TERRACOTTA_PINK, 0xf73897, 0xf73897),
+    GUNMETAL(dyeIndex + 13, "gunmetal", 0x5a5a5a, MapColor.TERRACOTTA_GRAY, 0x5a5a5a, 0x5a5a5a),
+    APPARITION(dyeIndex + 14, "apparition", 0xa2a2a2, MapColor.TERRACOTTA_LIGHT_GRAY, 0xa2a2a2, 0xa2a2a2),
+    NACRE(dyeIndex + 15, "nacre", 0xf7f7f7, MapColor.OFF_WHITE, 0xf7f7f7, 0xf7f7f7);
 
     private final int id;
     private final String name;
---
+++ src/main/java/fakezircon/classidyes/util/MiscVals.java
@@ -0,0 +1,8 @@
+package fakezircon.classidyes.util;
+
+import net.minecraft.util.DyeColor;
+
+public class MiscVals {
+    //for indexing mod dye colours based on a possibly extended DyeColor table
+    public static int dyeIndex = DyeColor.values().length;
+}
